bef31e32f70cc0d7d883cd82fb9b1b6df8cf8a27
mochitarou49 <21j5020@stu.meisei-u.ac.jp>
2023-06-01 19:47:37 +0900

    mochi

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let video = null;
     const init = async () => {
-     video = await loadVideo("SMILY.mp4");
+     video = await loadVideo("SMILY2.mp4");
      texture = new THREE.VideoTexture(video);
       video.play();
       video.pause();
