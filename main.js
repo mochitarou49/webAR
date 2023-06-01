686891e78fd4182a9da80040429afd5ddb5c58d7
mochitarou49 <21j5020@stu.meisei-u.ac.jp>
2023-06-01 19:57:51 +0900

    mochi

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-import {loadGLTF,loadVideo} from "./libs/loader.js";
+import { loadGLTF, loadVideo } from "./libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
 
   let video = null;
-    const init = async () => {
-     video = await loadVideo("SMILY2.mp4");
-     texture = new THREE.VideoTexture(video);
-      video.play();
-      video.pause();
-    }
+  const init = async () => {
+    video = await loadVideo("SMILY2.mp4");
+    texture = new THREE.VideoTexture(video);
+    video.play();
+    video.pause();
+  }
 
-  const start = async() => {
+
+
+  const start = async () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      
-    //   imageTargetSrc: './assets/targets/snow.mind',
-    imageTargetSrc: './assets/targets/targets.mind',
-      
+      imageTargetSrc: './assets/targets/snow.mind',
     });
-    const {renderer, scene, camera} = mindarThree;
+    const { renderer, scene, camera } = mindarThree;
 
+    // const video = await loadVideo("./assets/videos/sintel/snow.mp4");
     const video = await loadVideo("SMILY2.mp4");
     const texture = new THREE.VideoTexture(video);
 
     const geometry = new THREE.PlaneGeometry(1, 204 / 480);
     const material = new THREE.MeshBasicMaterial({ map: texture });
     const plane = new THREE.Mesh(geometry, material);
-    
+
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(plane);
 
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
       renderer.render(scene, camera);
     });
   }
-  start();
+  // start();
   const startButton = document.createElement("button");
   startButton.textContent = "Start";
   startButton.addEventListener("click", start);
