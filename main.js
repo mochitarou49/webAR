e3f81683658dbf4e14326ee4d16046d7f5155a66
mochitarou49 <21j5020@stu.meisei-u.ac.jp>
2023-06-15 06:15:13 +0900

    mochi

--- main.js
+++ main.js
@@ -3,51 +3,93 @@ const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  let video = null;
-  const init = async () => {
-    video = await loadVideo("Disney.mp4");
-    texture = new THREE.VideoTexture(video);
-    video.play();
-    video.pause();
-  }
+  // // let video0 = null;
+  // // const init1 = async () => {
+  // //   video0 = await loadVideo("Disney.mp4");
+  // //   texture = new THREE.VideoTexture(video0);
+  // //   video0.play();
+  // //   video0.pause();
+  // // }
+
+  // let video1 = null;
+  // const init2 = async () => {
+  //   video1 = await loadVideo("SMILY2.mp4");
+  //   texture = new THREE.VideoTexture(video1);
+  //   video1.play();
+  //   video1.pause();
+  // }
 //test
 
 
   const start = async () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/Disney.mind',
+      imageTargetSrc: './assets/targets/targets.mind',
     });
     const { renderer, scene, camera } = mindarThree;
 
-    const video = await loadVideo("Disney.mp4");
-    const texture = new THREE.VideoTexture(video);
+    // const mindarThree1 = new window.MINDAR.IMAGE.MindARThree({
+    //   container: document.body,
+    //   imageTargetSrc: './assets/targets/SMILY.mind',
+    // });
+    // const { renderer, scene, camera } = mindarThree1;
+    
+    
 
-    const geometry = new THREE.PlaneGeometry(1, 204 / 480);
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-    const plane = new THREE.Mesh(geometry, material);
+    const video0 = await loadVideo("Disney.mp4");
+    const video1 = await loadVideo("snow.mp4");
+    const texture0 = new THREE.VideoTexture(video0);
+    const texture1 = new THREE.VideoTexture(video1);
 
-    const anchor = mindarThree.addAnchor(0);
-    anchor.group.add(plane);
 
-    anchor.onTargetFound = () => {
-      video.play();
+    const geometry = new THREE.PlaneGeometry(1, 270 / 480);
+    const material0 = new THREE.MeshBasicMaterial({ map: texture0 });
+    const material1 = new THREE.MeshBasicMaterial({ map: texture1 });
+    const plane0 = new THREE.Mesh(geometry, material0);
+    const plane1 = new THREE.Mesh(geometry, material1);
+
+
+    const anchor0 = mindarThree.addAnchor(0);
+    const anchor1 = mindarThree.addAnchor(1);
+    anchor0.group.add(plane0);
+    anchor1.group.add(plane1);
+
+
+    anchor0.onTargetFound = () => {
+      video0.play();
     }
-    anchor.onTargetLost = () => {
-      video.pause();
+    anchor0.onTargetLost = () => {
+      video0.pause();
     }
-    video.addEventListener('play', () => {
-      video.currentTime = 6;
+    video0.addEventListener('play', () => {
+      video0.currentTime = 0;
+    });
+
+    anchor1.onTargetFound = () => {
+      video1.play();
+    }
+    anchor1.onTargetLost = () => {
+      video1.pause();
+    }
+    video1.addEventListener('play', () => {
+      video1.currentTime = 0;
     });
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
       renderer.render(scene, camera);
     });
+
+
   }
   // start();
   const startButton = document.createElement("button");
   startButton.textContent = "Start";
   startButton.addEventListener("click", start);
   document.body.appendChild(startButton);
+
+  const restartButton = document.createElement("button");
+  restartButton.textContent = "Restart";
+  restartButton.addEventListener("click", start);
+  document.body.appendChild(restartButton);
 });
